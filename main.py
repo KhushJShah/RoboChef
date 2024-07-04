2cfafba7e91ff2ba6b028b4cb5cae550367f3f67
Khush Shah <143137172+KhushJShah@users.noreply.github.com>
2024-07-05 04:19:50 +0530

    Update 07/04/2024

---
+++ main.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO # type: ignore
+
+# Load the pre-trained model
+model = YOLO('runs/detect/train3/weights/best.pt')
+yaml_path = 'spice_detection_dataset_yolo1/spice_detection_dataset_yolo1/data1.yaml'
+# Train the model
+model.train(data=yaml_path, epochs=20, imgsz=640)
+
+# Evaluate the model
+results = model.val()
+print(results)
