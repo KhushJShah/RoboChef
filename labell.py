a34764530e8f4560a800edc790626850083bfb6d
Khush Shah <143137172+KhushJShah@users.noreply.github.com>
2024-07-06 00:55:18 +0530

    Update 07/05/2024

---
+++ labell.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('E:/RoboChef/Annotated/Annotations_3.csv.csv')
+df.head()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO # type: ignore
 
 # Load the pre-trained model
 model = YOLO('runs/detect/train3/weights/best.pt')
-yaml_path = 'spice_detection_dataset_yolo1/spice_detection_dataset_yolo1/data1.yaml'
+yaml_path = 'E:/RoboChef/spice_detection_dataset_yolo1/spice_detection_dataset_yolo1/data1.yaml'
 # Train the model
 model.train(data=yaml_path, epochs=20, imgsz=640)
 
